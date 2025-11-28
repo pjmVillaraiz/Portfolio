3bb5ae8c164bc1d5115c4e73d3b67ff74bb6d63a
pjmVillaraiz <pjmvillaraiz@mcm.edu.ph>
2025-11-28 19:02:18 +0800

    Refactor theme toggle functionality into a function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
-});
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
